cd0020a819fbc7b12434bb10c8e41937e52a44cd
tongtiger <67530968+tongtiger@users.noreply.github.com>
2020-07-02 11:08:45 +0900

    Update playlist_name.py
    파이썬 객체를 저장하는 방법이 pickle이라는 모듈을 이용하는건데 이거 활용하면 좀 더 데이터 빨리 처리 가능할 듯

--- playlist_name.py
+++ playlist_name.py
@@ -5,13 +5,18 @@ with open('C:/Users/tongt/PycharmProjects/Melon/data/train.json', encoding='UTF8
 
 playlist_basket = []
 
+#플레이리스트 이름을 txt파일로 전환하기 위해 txt 파일 생성
+playlist_to_txt = open("C:/Users/tongt/PycharmProjects/Melon/data/playlist.txt", "w", encoding="UTF8") 
+
+#반복문, 조건문을 이용해 객체에 플레이리스트를 저장함과 동시에 txt 파일에 작성, 제목간 구분을 위해서 /n 삽입
 for i in range(len(train_data)):
     if train_data[i]['plylst_title'] in playlist_basket:
         pass
     else:
         playlist_basket.append(train_data[i]['plylst_title'])
-
-print(playlist_basket)
-
+        playlist_to_txt.write(train_data[i]['plylst_title'])
+        playlist_to_txt.write("/n")
+        
+playlist_to_txt.close()
 
 
