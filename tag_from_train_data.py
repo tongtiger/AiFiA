9d0853e4423ecdc5dfa890f98edfd4c850e26216
ckc0108 <67675259+ckc0108@users.noreply.github.com>
2020-07-12 18:35:26 +0900

    Add files via upload
    To 찬혁
    train_data에서 태그만 가져와서 저장까지

---
+++ tag_from_train_data.py
@@ -0,0 +1,23 @@
+import json
+import pickle
+import gzip
+
+with open('DataSet/train.json', encoding='UTF8') as train_data:
+    train = json.load(train_data)
+
+tag_list = []
+
+for i in range(len(train)):
+    tag_list.append(train[i]["tags"])
+
+# print(tag_list)
+
+'''
+# save
+with open('DataSet/tag_from_train_data.pickle', 'wb') as f:
+    pickle.dump(tag_list, f, pickle.HIGHEST_PROTOCOL)
+'''
+
+# save and compress
+with gzip.open('DataSet/tag_from_train_data.pickle', 'wb') as f:
+    pickle.dump(tag_list, f)
