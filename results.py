326ec213d78790984794e9496ad95903e9354599
tongtiger <67530968+tongtiger@users.noreply.github.com>
2020-07-19 22:35:19 +0900

    while 구문이 안먹히네

--- results.py
+++ results.py
@@ -27,13 +27,12 @@ results = []
 #def type1_presager(data, ...):
 
 def type2_presager(topn):
-    predictions = fasttext.most_similar(positive=[word for word in data['plylst_title'].split(" ")], topn=topn)
-    while len(result['tags']) < 10:
-        for prediction in predictions:
-            if prediction[0] in tag_list:
-                result['tags'].append(prediction[0])
-            else:
-                pass
+    predictions = fasttext.most_similar(positive=[word for word in data['plylst_title'].split(" ")], topn=topn)    
+    for prediction in predictions:
+        if prediction[0] in tag_list:
+            result['tags'].append(prediction[0])
+        else:
+            pass
 
 def type2_presager_ngram(topn):
     predictions = fasttext.most_similar(positive=[word for word in data['plylst_title'].split(" ")], topn=topn)
