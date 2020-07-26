1c2f92848734f5e901a3b68ef0355803241e98c7
tongtiger <67530968+tongtiger@users.noreply.github.com>
2020-07-26 18:23:03 +0900

    Update results.py

--- results.py
+++ results.py
@@ -6,13 +6,13 @@ from tqdm import tqdm
 #필요한 자료 및 모델
 val_data = json.load(open("data/val.json", "rb"))   #맞춰야 하는 데이터
 
-with open("tag_name_list.p", "rb") as f:
+with open("tag_name.list", "rb") as f:
     tag_list = pickle.load(f)   #태그 리스트
 
-with open("music_tag_dic.p", "rb") as f:
+with open("music_tag.dic", "rb") as f:
     music_tag = pickle.load(f)  #태그를 음악에 따라 묶은 것
 
-with open("data/tag_music_freq.dic", "rb") as f:
+with open("tag_music_freq.dic", "rb") as f:
     tag_music_freq = pickle.load(f)
 
 fasttext = FastText.load("FastText.model")
@@ -20,9 +20,6 @@ fasttext = FastText.load("FastText.model")
 
 #예상 내용이 들어가는 리스트, 일단 태그 10개, 곡 100개 만족하는거는 나중에 따로 처리해주고, 일단 채워넣는 단계
 results = []
-
-
-
 
 #def type1_presager(data, ...):
 
@@ -34,31 +31,14 @@ def type1_vote(frequency):
             pass
 
 def type1_submit_answer():
-    for answer_tag in answer_tags[:99 + 1]:
+    for answer_tag in answer_tags[:9+ 1]:
         result['tags'].append(answer_tag)
 
 def type2_presager(topn):
     predictions = fasttext.most_similar(positive=[word for word in data['plylst_title'].split(" ")], topn=topn)
-    while len(result['tags']) < 10:
-        for prediction in predictions:
-            if prediction[0] in tag_list:
-                result['tags'].append(prediction[0])
-            else:
-                pass
-
-def type2_presager_ngram(topn):
-    predictions = fasttext.most_similar(positive=[word for word in data['plylst_title'].split(" ")], topn=topn)
     for prediction in predictions:
         if prediction[0] in tag_list:
-            if len(result['tags']) == 0:
-                result['tags'].append(prediction[0])
-            else:
-                for begin_tag in result['tags']:
-                    sim1, sim2 = diff_2gram(begin_tag, prediction[0])
-                    if sim1 <= 0.5 and sim2 <= 0.5:
-                        result['tags'].append(prediction[0])
-                    else:
-                        pass
+            result['tags'].append(prediction[0])
         else:
             pass
 
@@ -73,32 +53,6 @@ def type2_submit_answer():
     for answer_song in answer_songs[:99 + 1]:
         result['songs'].append(answer_song)
 
-def ngram(s, num):
-   res = []
-   letter = 0
-   slen = len(s) - num + 1
-   for i in range(slen):
-        ss = s[i:i + num]
-        res.append(ss)
-   return res
-
-
-def diff_2gram(sa, sb):
-    if len(sa) == 1 or len(sb) == 1:
-        a = ngram(sa, 1)
-        b = ngram(sb, 1)
-    else:
-        a = ngram(sa, 2)
-        b = ngram(sb, 2)
-    r = []
-    count = 0
-    for i in a:
-        for j in b:
-            if i == j:
-                count += 1
-                r.append(i)
-
-    return count / len(a), count/len(b)
 
 def type3_presager(topn):
     pre_predictions.clear()
@@ -116,14 +70,6 @@ def type4_presager(topn):
             result['tags'].append(prediction[0])
         else:
             pass
-
-'''def type4_presager(topn):
-    predictions = fasttext.most_similar(positive=[tag for tag in data['tags']], topn=topn)
-    for prediction in predictions:
-        if prediction[0] in tag_list:
-            result['tags'].append(prediction[0])
-        else:
-            pass'''
 
 
 #위에서 정의한 함수 4개는 아래와 같이 사용될 예정, topn만 조정하여 태그 개수 충족
@@ -143,40 +89,40 @@ for data in tqdm(val_data):
 
         answer_tags = []
         type1_vote(frequency=10)
-        if len(answer_tags) >= 100:
+        if len(answer_tags) >= 10:
             type1_submit_answer()
         else:
             type1_vote(frequency=9)
-            if len(answer_tags) >= 100:
+            if len(answer_tags) >= 10:
                 type1_submit_answer()
             else:
                 type1_vote(frequency=8)
-                if len(answer_tags) >= 100:
+                if len(answer_tags) >= 10:
                     type1_submit_answer()
                 else:
                     type1_vote(frequency=7)
-                    if len(answer_tags) >= 100:
+                    if len(answer_tags) >= 10:
                         type1_submit_answer()
                     else:
                         type1_vote(frequency=6)
-                        if len(answer_tags) >= 100:
+                        if len(answer_tags) >= 10:
                             type1_submit_answer()
                         else:
                             type1_vote(frequency=5)
-                            if len(answer_tags) >= 100:
+                            if len(answer_tags) >= 10:
                                 type1_submit_answer()
                             else:
                                 type1_vote(frequency=4)
-                                if len(answer_tags) >= 100:
+                                if len(answer_tags) >= 10:
                                     type1_submit_answer()
 
                                 else:
                                     type1_vote(frequency=3)
-                                    if len(answer_tags) >= 100:
+                                    if len(answer_tags) >= 10:
                                         type1_submit_answer()
                                     else:
                                         type1_vote(frequency=2)
-                                        if len(answer_tags) >= 100:
+                                        if len(answer_tags) >= 10:
                                             type1_submit_answer()
                                         else:
                                             type1_vote(frequency=1)
@@ -185,7 +131,7 @@ for data in tqdm(val_data):
         # 투표
         musics = {}
         for tag in result['tags']:
-            for song in tag_music_freq[tag]:
+            for song in tag_music_freq[tag][:500]:
                 if song not in musics:
                     musics[song] = 1
                 else:
@@ -233,46 +179,41 @@ for data in tqdm(val_data):
                                             type2_vote(frequency=1)
                                             type2_submit_answer()
 
-
-
     #type2 problem
     elif data['tags'] == data['songs'] == []:
-        type2_presager(topn=20)
+        type2_presager(topn=100)
         if len(result['tags']) >= 10:
             result['tags'] = result['tags'][:9 + 1]
         else:
             result['tags'].clear()
-            type2_presager(topn=30)
+            type2_presager(topn=200)
             if len(result['tags']) >= 10:
                 result['tags'] = result['tags'][:9 + 1]
             else:
                 result['tags'].clear()
-                type2_presager(topn=40)
+                type2_presager(topn=300)
                 if len(result['tags']) >= 10:
                     result['tags'] = result['tags'][: 9 + 1]
                 else:
                     result['tags'].clear()
-                    type2_presager(topn=50)
+                    type2_presager(topn=400)
                     if len(result['tags']) >= 10:
                         result['tags'] = result['tags'][: 9 + 1]
                     else:
                         result['tags'].clear()
-                        type2_presager(topn=100)
+                        type2_presager(topn=500)
                         if len(result['tags']) >= 10:
                             result['tags'] = result['tags'][: 9 + 1]
                         else:
                             result['tags'].clear()
-                            type2_presager_(topn=150)
-                            if len(result['tags']) >= 10:
-                                result['tags'] = result['tags'][: 9 + 1]
-                            else:
-                                result['tags'].clear()
-                                type2_presager(topn=4000)
+                            type2_presager(topn=5000)
+                            result['tags'] = result['tags'][: 9 + 1]
+
 
         # 투표
         musics = {}
         for tag in result['tags']:
-            for song in tag_music_freq[tag]:
+            for song in tag_music_freq[tag][:500]:
                 if song not in musics:
                     musics[song] = 1
                 else:
@@ -368,12 +309,12 @@ for data in tqdm(val_data):
                         for i in range(10):
                             result['tags'].append(pre_predictions[i])
                     else:
-                        type3_presager(topn=4000)
+                        type3_presager(topn=5000)
 
         # 투표
         musics = {}
         for tag in result['tags']:
-            for song in tag_music_freq[tag]:
+            for song in tag_music_freq[tag][:500]:
                 if song not in musics:
                     musics[song] = 1
                 else:
@@ -452,7 +393,7 @@ for data in tqdm(val_data):
         # 투표
         musics = {}
         for tag in result['tags']:
-            for song in tag_music_freq[tag]:
+            for song in tag_music_freq[tag][:500]:
                 if song not in musics:
                     musics[song] = 1
                 else:
@@ -506,6 +447,6 @@ for data in tqdm(val_data):
     results.append(result)
 
 
-with open("data/results.json", "wb", encoding='UTF8') as f:
+with open("data/results.json", "w", encoding='UTF8') as f:
     json.dump(results, f, ensure_ascii=False)
 
